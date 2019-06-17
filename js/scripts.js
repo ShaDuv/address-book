bc3b8c517b4ed016cef6b760b9f9df5ffad22796
Hannah Melendy and Shawna Duvall <shawna@>
2019-06-17 10:44:30 -0700

    add code to allow contact to be added and name will appear in list of contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,19 @@
 // Business Logic for AddressBook ---------
-//AddressBook constructor
 function AddressBook() {
   this.contacts = [],
-  // instantiate new AddressBooks with a currentId property
   this.currentId = 0
 }
 
-//prototype method to add new Contacts to an AddressBook
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
-//ensure each new Contact added to the AddressBook has a unique ID
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
 
-//finding Contacts
 AddressBook.prototype.findContact = function(id) {
   for (var i=0; i< this.contacts.length; i++) {
     if (this.contacts[i]) {
@@ -30,7 +25,6 @@ AddressBook.prototype.findContact = function(id) {
   return false;
 }
 
-//deleting Contacts
 AddressBook.prototype.deleteContact = function(id) {
   for (var i=0; i< this.contacts.length; i++) {
     if (this.contacts[i]) {
@@ -43,16 +37,37 @@ AddressBook.prototype.deleteContact = function(id) {
   return false;
 }
 
-
 // Business Logic for Contacts ---------
-//contact constructor
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.phoneNumber = phoneNumber
 }
 
-//prototype method on Contact constructor
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+// User Interface Logic ---------
+var addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  })
+})
